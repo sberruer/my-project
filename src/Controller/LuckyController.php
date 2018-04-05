718c57ebbbb413322633d856f5ca50d1277627ba
sebastien.berruer <sebastien.berruer@adventiel.fr>
2018-04-05 12:51:33 +0200

    Ajout du template pour le controller LuckyController

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -3,17 +3,17 @@ namespace App\Controller;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class LuckyController
+class LuckyController extends Controller
 {
     /**
      * @Route("/lucky/number")
-     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function number() {
         $number = mt_rand(0, 100);
         
-        return new Response("<html><body>Lucky number: $number</body></html>");
+        return $this->render("lucky/number.html.twig", array('number' => $number,));
     }
 }
 
