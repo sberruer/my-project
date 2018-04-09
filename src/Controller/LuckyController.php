707f8b8f186d33df9f7c3c7430a2e94af2813e39
sebastien.berruer <sebastien.berruer@adventiel.fr>
2018-04-09 12:32:50 +0200

    Ajout d'une méthode avec injection de service

--- src/Controller/LuckyController.php
+++ src/Controller/LuckyController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Psr\Log\LoggerInterface;
 
 class LuckyController extends Controller
 {
@@ -12,6 +13,17 @@ class LuckyController extends Controller
      */
     public function number() {
         $number = mt_rand(0, 100);
+        
+        return $this->render("lucky/number.html.twig", ['number' => $number]);
+    }
+    
+    /**
+     * @Route("/lucky/number/{max}")
+     */
+    public function numberWithMax($max, LoggerInterface $logger) {
+        $number = mt_rand(0, $max);
+        
+        $logger->info("We are logging");
         
         return $this->render("lucky/number.html.twig", ['number' => $number]);
     }
