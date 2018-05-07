f2d5ee6430de9c47c9d2e733ae37288366945be0
sebastien.berruer <sebastien.berruer@adventiel.fr>
2018-05-07 13:34:23 +0200

    Mise à jour de l'entité à l'aide de maker

--- src/Entity/Product.php
+++ src/Entity/Product.php
@@ -16,8 +16,42 @@ class Product
      */
     private $id;
 
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $price;
+
     public function getId()
     {
         return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getPrice(): ?int
+    {
+        return $this->price;
+    }
+
+    public function setPrice(int $price): self
+    {
+        $this->price = $price;
+
+        return $this;
     }
 }
