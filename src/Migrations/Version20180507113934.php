1f672e6c1f61f7de4f8c5c487ce891226e5688c7
sebastien.berruer <sebastien.berruer@adventiel.fr>
2018-05-07 13:40:08 +0200

    Mise à jour de l'entité et des scripts de migration

---
+++ src/Migrations/Version20180507113934.php
@@ -0,0 +1,32 @@
+<?php declare(strict_types = 1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Migrations\AbstractMigration;
+use Doctrine\DBAL\Schema\Schema;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+class Version20180507113934 extends AbstractMigration
+{
+    public function up(Schema $schema)
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('ALTER TABLE product ADD COLUMN description CLOB NOT NULL');
+    }
+
+    public function down(Schema $schema)
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'sqlite', 'Migration can only be executed safely on \'sqlite\'.');
+
+        $this->addSql('CREATE TEMPORARY TABLE __temp__product AS SELECT id, name, price FROM product');
+        $this->addSql('DROP TABLE product');
+        $this->addSql('CREATE TABLE product (id INTEGER NOT NULL, name VARCHAR(255) NOT NULL, price INTEGER NOT NULL, PRIMARY KEY(id))');
+        $this->addSql('INSERT INTO product (id, name, price) SELECT id, name, price FROM __temp__product');
+        $this->addSql('DROP TABLE __temp__product');
+    }
+}
